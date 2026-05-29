9dca78b0ca6f31654b4acb6cf0a005f115fd8887
Jalen Bridges <34227411+cbridges1@users.noreply.github.com>
2026-05-29 14:09:35 -0500

    Removed .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/jalenbridges/Source/portfolio/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jalenbridges/Source/portfolio/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jalenbridges/Source/portfolio/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jalenbridges/Source/portfolio/src/css/custom.css"),
-];
